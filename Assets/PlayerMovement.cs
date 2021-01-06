c7cfaf847c6b1e00dc9c2d7ea37bcae5fc36d5b7
Carlos Alexandre Zgierski <carloszgierski@gmail.com>
2021-01-05 21:57:55 -0300

    Changed movespeed and added run/stamina mechanic.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private CharacterController characterController;
-    [SerializeField] private float forwardMoveSpeed = 12f;
+    [SerializeField] private float forwardMoveSpeed = 6f;
     [SerializeField] private float backwardMoveSpeed = 3.5f;
     [SerializeField] private float gravity = -9.81f;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundDistance = 0.4f;
     [SerializeField] private LayerMask groundMask;
-    
+    [SerializeField] private float totalStamina = 100f;
+    [SerializeField] private float currentStamina = 100f;
     private Vector3 velocity;
     private bool isGrounded;
+    [SerializeField] private bool isRunning = false;
+    [SerializeField] private bool canRun = true;
 
     // Update is called once per frame
     void Update()
@@ -21,7 +24,7 @@ public class PlayerMovement : MonoBehaviour
 
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if(isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
@@ -31,10 +34,57 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 movement = transform.right * horizontalMovement + transform.forward * verticalMovement;
 
-        characterController.Move(movement * forwardMoveSpeed * Time.deltaTime);
+        float moveSpeedToUse = (verticalMovement >= 0) ? forwardMoveSpeed : backwardMoveSpeed;
+
+
+        if (canRun)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                if (currentStamina > 0) 
+                { 
+                    isRunning = true;
+                    forwardMoveSpeed = 12f;
+                }
+
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftShift) || currentStamina <= 0)
+            {
+                StartCoroutine(RunningCooldown());
+                isRunning = false;
+                forwardMoveSpeed = 6f;
+            }
+        }
+
+        if (isRunning)
+        {
+            currentStamina -= 30 * Time.deltaTime;
+        }
+        else
+        {
+            if (currentStamina < totalStamina)
+            {
+                currentStamina += 15 * Time.deltaTime;
+            }
+        }
+
+        characterController.Move(movement * moveSpeedToUse * Time.deltaTime);
 
         velocity.y += gravity * Time.deltaTime;
 
         characterController.Move(velocity * Time.deltaTime);
+
     }
+
+    IEnumerator RunningCooldown()
+    {
+        canRun = false;
+        yield return new WaitForSeconds(2.5f);
+        if (currentStamina > 0)
+        {
+            canRun = true;
+        }
+    }
+
 }
